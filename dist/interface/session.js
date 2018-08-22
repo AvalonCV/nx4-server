54b6820f0c66db29d994217a6fb47c9bfcc11286
AvalonCV <avaloncv@gmail.com>
2018-08-22 07:11:49 +0200

    ... and dist

--- dist/interface/session.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleAction = (session, action_type, payload) => {
-    console.log('hier', session, action_type, payload);
-};
-//# sourceMappingURL=session.js.map
